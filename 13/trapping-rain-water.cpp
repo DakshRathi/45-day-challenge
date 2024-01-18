f8ed9f7fb37cbb6ca9d439f9db1a34b48fd6b7f4
DakshRathi <117447434+DakshRathi@users.noreply.github.com>
2024-01-18 17:47:12 +0530

    Day 13 done

---
+++ 13/trapping-rain-water.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        
+        int l = 0, r = height.size() - 1, lmax = INT_MIN, rmax = INT_MIN, ans = 0;
+        while (l < r)
+        {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+            ans += (lmax < rmax) ? lmax - height[l++] : rmax - height[r--];
+        }
+        return ans;
+    }
+};
